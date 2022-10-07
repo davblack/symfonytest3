38518a1124cd4f0271d76759fb6b5f73232cf808
davblack <davidfekete51@gmail.com>
2022-10-07 14:57:54 +0200

    adeed some twig stuff

--- src/Controller/QuestionController.php
+++ src/Controller/QuestionController.php
@@ -2,26 +2,34 @@
 
 namespace App\Controller;
 
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
-class QuestionController
+class QuestionController extends AbstractController
+
 {
     /**
      * @Route("/")
      */
     public function homepage(){
-        return new Response('asd');
+        return new Response('controller');
     }
 
     /**
-     * @Route("/questions/{mizus}")
+     * @Route("/questions/{slug}")
      */
-    public function show($mizus): Response
+    public function show($slug)
     {
-        return new Response(sprintf(
-            'smth smth"%s"',
-            ucwords(str_replace('-', ' ', $mizus))
-        ));
+        $answers = [
+            'answer 1',
+            'answer 2',
+            'answer 3',
+            ];
+        return $this->render('question/show.html.twig',[
+
+            'question' =>ucwords(str_replace('-', ' ', $slug)),
+            'answers' =>$answers,
+            ]);
     }
 }
